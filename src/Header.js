3f41c9565a372f569b13648c5015a3d4f920d6de
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2024-01-02 20:43:30 -0800

    app css

--- src/Header.js
+++ src/Header.js
@@ -1,43 +1,17 @@
-import GoogleMapReact from "google-map-react";
-import LocationMarker from "./LocationMarker";
-import LocationInfo from "./LocationInfo";
-import { useState } from "react";
+import { Icon } from "@iconify/react";
+import locationIcon from "@iconify/icons-mdi/fire-alert";
 
-const Map = ({ eventData, center, zoom }) => {
-  const [LocInfo, setLocInfo] = useState(null);
-  const markers = eventData.map((ev) => {
-    if (ev.categories[0].id === 8) {
-      return (
-        <LocationMarker
-          lat={ev.geometries[0].coordinates[1]}
-          lng={ev.geometries[0].coordinates[0]}
-          onClick={() => setLocInfo({ id: ev.id, title: ev.title })}
-        />
-      );
-    }
-    return null;
-  });
+const Header = () => {
   return (
-    <div className="map">
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyDUlM1SGGfU3lntGSgv_9eXeonUlToaDyI" }} // Enter your Google Maps API key here
-        defaultCenter={center}
-        defaultZoom={zoom}
-      >
-        {markers}
-        <LocationMarker lat={center.lat} lng={center.lng} />
-      </GoogleMapReact>
-      {LocInfo && <LocationInfo info={LocInfo} />}
+    <div className="header">
+      <h1>
+        <Icon icon={locationIcon} className="header-icon" />
+        WildFire Tracker
+        <Icon icon={locationIcon} className="header-icon" />
+      </h1>
+      <p>Data Provided by NASA</p>
     </div>
   );
 };
 
-Map.defaultProps = {
-  center: {
-    lat: 42.3265,
-    lng: -122.8765,
-  },
-  zoom: 6,
-};
-
-export default Map;
+export default Header;
