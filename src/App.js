795e60bdff1d0019a29ced6372d02a7954a02f81
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2024-01-02 20:48:03 -0800

    app js update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   useEffect(() => {
     const fetchEvents = async () => {
       setLoading(true);
-      const res = await fetch("https://eonet.gsfc.nasa.gov/api/v2.1/events");
+      const res = await fetch("");
       const { events } = await res.json();
 
       setEventData(events);
